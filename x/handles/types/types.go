078fe468a0dfe1a52bd8eeaaf1f50e2be60a4177
Emanuele Gaspari <inmarelibero@gmail.com>
2025-05-13 22:20:23 +0200

    built

--- x/handles/types/types.go
+++ x/handles/types/types.go
@@ -1,1 +1,7 @@
 package types
+
+type Account struct {
+	Id     uint64 `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
+	Owner  string `protobuf:"bytes,2,opt,name=user,proto3" json:"owner,omitempty"`
+	Handle string `protobuf:"bytes,3,opt,name=handle,proto3" json:"handle,omitempty"`
+}
